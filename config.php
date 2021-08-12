b84222e5fdd6e5133d4a8a320f24173838eb48e6
Valestie25 <64401227+Valestie25@users.noreply.github.com>
2021-08-11 17:37:26 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$db = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'port' => '3306',
+    'db' => 'terrarium' //Cambiar al nombre de tu base de datos
+];
